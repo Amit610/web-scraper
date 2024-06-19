085accb00df382e54379901a7dfa6d7642405626
Amit <amitjadhav610@gmail.com>
2024-06-19 14:48:42 +0530

    B

--- server.js
+++ server.js
@@ -104,16 +104,6 @@ app.post("/scrape", async (req, res) => {
   try {
     browser = await puppeteer.launch({
       headless: false,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        // "--disable-dev-shm-usage",
-        // "--disable-accelerated-2d-canvas",
-        // "--no-first-run",
-        "--no-zygote",
-        // "--single-process",
-        "--disable-gpu",
-      ],
       executablePath:
         process.env.NODE_ENV === "production"
           ? process.env.PUPPETEER_EXECUTABLE_PATH
