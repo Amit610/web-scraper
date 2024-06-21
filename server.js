57098deaadad4eb4313eee48f0e37ed0d58f51d6
Amit <amitjadhav610@gmail.com>
2024-06-21 16:28:57 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 const express = require("express");
-// const chromium = require("@sparticuz/chromium");
+const chromium = require("@sparticuz/chromium");
 const xlsx = require("xlsx");
 const path = require("path");
 require("dotenv").config();
@@ -118,9 +118,9 @@ app.post("/scrape", async (req, res) => {
         process.env.NODE_ENV === "production"
           ? process.env.PUPPETEER_EXECUTABLE_PATH
           : puppeteer.executablePath(),
-      // args: chromium.args,
-      // defaultViewport: chromium.defaultViewport,
-      // executablePath: await chromium.executablePath(),
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
       ignoreHTTPSErrors: true,
     });
 
